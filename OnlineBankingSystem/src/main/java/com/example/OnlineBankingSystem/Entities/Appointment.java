a82183b619d56781df985b793793ac47cf421454
ISBAINE MOHAMED <isbainemouhamed@gmailcom>
2021-12-24 20:02:42 +0100

    first commit1

--- OnlineBankingSystem/src/main/java/com/example/OnlineBankingSystem/Entities/Appointment.java
+++ OnlineBankingSystem/src/main/java/com/example/OnlineBankingSystem/Entities/Appointment.java
@@ -3,9 +3,12 @@ package com.example.OnlineBankingSystem.Entities;
 import javax.persistence.*;
 import java.util.Date;
 
+
+//cette classe représente un rendez-vous de l'utilisateur
 @Entity
 public class Appointment {
 
+    // Chaque rdv a son propre id , Date, localisation, description et un champs boolean pour dire si le rdv a été confirmé ou non
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -14,10 +17,14 @@ public class Appointment {
     private String description;
     private boolean confirmed;
 
+    // Un rdv peut appartenir à un seul utilsateur
+    //l'annotation @ManyToOne permet de créer une colonne ayant l'id de la table user ( qui portera le nom user_id)
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
+
+    // ## Getters et Setters ##
     public Long getId() {
         return id;
     }
@@ -68,10 +75,10 @@ public class Appointment {
 
     @Override
     public String toString() {
-        return "Appointment{" +
-                "id=" + id +
+        return "Rendez-vous:{" +
+                "numéro=" + id +
                 ", date=" + date +
-                ", location='" + location + '\'' +
+                ", localisation='" + location + '\'' +
                 ", description='" + description + '\'' +
                 ", user=" + user +
                 '}';
